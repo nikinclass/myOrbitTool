5feb4e09566fe4365a2a3cdc66782c5055da90e7
Nikki <ngabor@mail.weber.edu>
2025-09-09 14:00:39 -0600

    Satellite and station api stubs created

--- api/routes/scenario.ts
+++ api/routes/scenario.ts
@@ -1,4 +1,6 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body, checkSchema, validationResult } from "express-validator";
+// import { body } from "express-validator";
 const router = Router();
 
 // POST api/scenario
@@ -9,5 +11,59 @@ router.post("/", async (req, res) => {
   //return scenario id
   res.status(200).json({ id: id });
 });
+const createSatelliteChain = () => {
+  return body(["tle_line0", "tle_line1", "tle_line2"])
+    .notEmpty()
+    .withMessage("TLE line cannot be empty!");
+};
+
+router.post(
+  "/satellite",
+  createSatelliteChain(),
+  async (req: Request, res: Response) => {
+    //this will make a new satellite
+    const result = validationResult(req);
+    console.log(result);
+    if (result.isEmpty()) {
+      return res.send(200);
+    }
+    res.status(400).json({ errors: result.array() });
+  }
+);
+
+const createStationChain = () => {
+  return [
+    body(["name"])
+      .notEmpty()
+      .withMessage("Ground station name cannot be empty!"),
+    body("latitude")
+      .notEmpty()
+      .isFloat()
+      .withMessage("Latitude must be a floating point number"),
+    body("longitude")
+      .notEmpty()
+      .isFloat()
+      .withMessage("Longitude must be a floating point number"),
+    body("altitude")
+      .notEmpty()
+      .isFloat()
+      .withMessage("Altitude must be a floating point number"),
+  ];
+};
+
+router.post(
+  "/station",
+  createStationChain(),
+  async (req: Request, res: Response) => {
+    //this will make a new station
+    const result = validationResult(req);
+    console.log("Hey!!");
+    console.log(result);
+    if (result.isEmpty()) {
+      return res.send(200);
+    }
+    res.status(400).json({ errors: result.array() });
+  }
+);
 
 export = router;
--- ui/src/pages/Scenario.tsx
+++ ui/src/pages/Scenario.tsx
@@ -1,26 +1,131 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 const PROXIED_URL = "/api/scenario";
 const LOCALHOST_URL = "http://localhost:8080/api/scenario";
 
 export function Scenario() {
+  const [stationName, setStationName] = useState<string>("");
+  const [stationLatitude, setStationLatitude] = useState<number>(0);
+  const [stationLongitude, setStationLongitude] = useState<number>(0);
+  const [stationAltitude, setStationAltitude] = useState<number>(0);
+
+  const [tleLine0, settleLine0] = useState<string>("");
+  const [tleLine1, settleLine1] = useState<string>("");
+  const [tleLine2, settleLine2] = useState<string>("");
+
   const navigate = useNavigate();
+
+  const onSatelliteAdd = async () => {
+    const payload = {
+      tle_line0: tleLine0,
+      tle_line1: tleLine1,
+      tle_line2: tleLine2,
+    };
+    fetch(`${LOCALHOST_URL}/satellite`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+  };
+
+  const onStationAdd = async () => {
+    const payload = {
+      name: stationName,
+      latitude: stationLatitude,
+      longitude: stationLongitude,
+      altitude: stationAltitude,
+    };
+    console.log("hey!");
+    fetch(`${LOCALHOST_URL}/station`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+  };
+
   return (
     <div className="flex flex-col border p-4 bg-green-500">
       <h1>Scenario Page</h1>
       <div className="flex flex-col border-red-500 gap-2">
         <h3>Add a Satellite</h3>
-        <input className="bg-white" type="text" placeholder="tle line 0" />
-        <input className="bg-white" type="text" placeholder="tle line 1" />
-        <input className="bg-white" type="text" placeholder="tle line 2" />
-        <button className="border rounded-full">Add</button>
+        <input
+          className="bg-white"
+          type="text"
+          placeholder="tle line 0"
+          value={tleLine0}
+          onChange={(e) => {
+            settleLine0(e.target.value);
+          }}
+        />
+        <input
+          className="bg-white"
+          type="text"
+          placeholder="tle line 1"
+          value={tleLine1}
+          onChange={(e) => {
+            settleLine1(e.target.value);
+          }}
+        />
+        <input
+          className="bg-white"
+          type="text"
+          placeholder="tle line 2"
+          value={tleLine2}
+          onChange={(e) => {
+            settleLine2(e.target.value);
+          }}
+        />
+        <button className="border rounded-full" onClick={onSatelliteAdd}>
+          Add
+        </button>
       </div>
       <div className="flex flex-col border-red-500 gap-2">
         <h3>Add a Ground Station</h3>
-        <input className="bg-white" type="text" placeholder="Latitude" />
-        <input className="bg-white" type="text" placeholder="Longitude" />
-        <input className="bg-white" type="text" placeholder="Altitude" />
-        <button className="border rounded-full">Add</button>
+        <input
+          className="bg-white"
+          type="text"
+          placeholder="Name"
+          value={stationName}
+          onChange={(e) => {
+            setStationName(e.target.value);
+          }}
+        />
+        <input
+          className="bg-white"
+          placeholder="Latitude"
+          type="number"
+          value={stationLatitude}
+          onChange={(e) => {
+            setStationLatitude(e.target.valueAsNumber);
+          }}
+        />
+        <input
+          className="bg-white"
+          type="number"
+          placeholder="Longitude"
+          value={stationLongitude}
+          onChange={(e) => {
+            setStationLongitude(e.target.valueAsNumber);
+          }}
+        />
+        <input
+          className="bg-white"
+          type="number"
+          placeholder="Altitude"
+          value={stationAltitude}
+          onChange={(e) => {
+            setStationAltitude(e.target.valueAsNumber);
+          }}
+        />
+        <button className="border rounded-full" onClick={onStationAdd}>
+          Add
+        </button>
       </div>
     </div>
   );
