b7c81dd25081defd60a28a42d9a663e8cdd6ce9f
Nikki <ngabor@mail.weber.edu>
2025-09-12 11:09:42 -0600

    search effects and additions

--- ui/src/components/Search.tsx
+++ ui/src/components/Search.tsx
@@ -3,14 +3,30 @@ import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 
+const PROXIED_URL = "/api/satellites";
+const LOCALHOST_URL = "http://localhost:8080/api/satellites";
+
 export function Search({ items }: { items: [] }) {
   const [search, setSearch] = useState<string>();
+  const [filteredItems, setFilteredItems] = useState<[]>();
+  useEffect(() => {
+    const getSearchItems = async () => {
+      try {
+        const response = await fetch(
+          `${LOCALHOST_URL}?filter=${search?.toLowerCase()}`
+        );
+        const payload = await response.json();
+        console.log(payload);
+      } catch (err: unknown) {}
+    };
+    getSearchItems();
+  }, [search]);
 
   return (
-    <div className="text-card-foreground rounded-lg overflow-hidden">
+    <div className="text-card-foreground rounded-lg overflow-hidden w-[200px]">
       <div
         className={cn(
-          "flex gap-1 justify-center items-center bg-card ",
+          "flex gap-1 items-center bg-card ",
           "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30  flex h-9 w-full min-w-0 rounded-md px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           `${search && search.length > 0 ? "rounded-b-none border-b" : ""}`
@@ -32,20 +48,27 @@ export function Search({ items }: { items: [] }) {
         />
       </div>
       {search && search.length > 0 && (
-        <div className="flex flex-col gap-1 text-sm bg-card p-2">
-          {items.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="p-1 pr-4 pl-4 hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 rounded-lg w-full h-fit"
-              >
-                <div className="[&:not(:last-child)]:border-b flex">
-                  <p className="flex-1">Item</p>
-                  <Plus className="self-center" size={16} />
+        <div className="flex flex-col gap-1 text-sm bg-card p-2 ">
+          {filteredItems &&
+            filteredItems.length > 0 &&
+            items.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="p-1 pr-4 pl-4 hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 rounded-lg w-full h-fit"
+                >
+                  <div className="[&:not(:last-child)]:border-b flex">
+                    <p className="flex-1">Item</p>
+                    <Plus className="self-center" size={16} />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          {!(filteredItems && filteredItems.length > 0) && (
+            <div className="p-1 pr-4 pl-4 rounded-lg w-full h-fit">
+              No matching satellite found.
+            </div>
+          )}
         </div>
       )}
     </div>
