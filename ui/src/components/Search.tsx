3bfd4e747c794f0ebd19df8c7dec5d8956ad8642
joshkrutz <jfkrutz@gmail.com>
2025-09-12 08:50:27 -0700

    ASDF

--- ui/src/components/Search.tsx
+++ ui/src/components/Search.tsx
@@ -11,7 +11,7 @@ export function Search({ items }: { items: [] }) {
       <div
         className={cn(
           "flex gap-1 justify-center items-center bg-card ",
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-card selection:text-primary-foreground dark:bg-input/30  flex h-9 w-full min-w-0 rounded-md px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30  flex h-9 w-full min-w-0 rounded-md px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           `${search && search.length > 0 ? "rounded-b-none border-b" : ""}`
         )}
