2ec3976ec8e814a6155dbc483abf270efe9d1410
PaytonORogers <payton.o.rogers@gmail.com>
2025-09-15 09:48:46 -0700

    pushed again

--- api/routes/scenario.ts
+++ api/routes/scenario.ts
@@ -113,7 +113,7 @@ router.get("/refresh", (req, res) => {
   res.status(200);
 });
 
-router.post("/site", (req, res) => {
+router.post("/siteczml", (req, res) => {
   var czml = siteCzmlConverter(req.body);
   res.status(200).json(czml);
 });
