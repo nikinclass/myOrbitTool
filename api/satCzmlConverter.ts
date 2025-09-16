37366c5c5b412b054ec12efec8c58d897b86626d
joshkrutz <jfkrutz@gmail.com>
2025-09-16 09:28:16 -0700

    asdf

--- api/satCzmlConverter.ts
+++ api/satCzmlConverter.ts
@@ -1,46 +1,48 @@
 import { Satellite } from "./types";
 
-import { twoline2satrec, sgp4, json2satrec, OMMJsonObjectV3 } from "satellite.js";
+import {
+  twoline2satrec,
+  sgp4,
+  json2satrec,
+  OMMJsonObjectV3,
+} from "satellite.js";
 import moment from "moment";
 const julian = require("julian");
 // https://github.com/r3lek/tle2czml/blob/master/index.js#L105
 
-const satCzmlConverter = (
-  sat: Satellite,
-) => {
-
+const satCzmlConverter = (sat: Satellite) => {
   let res = []; //result for position
   //Set satrec
 
   const omm = {
-    "OBJECT_NAME": sat["OBJECT_NAME"],
-    "OBJECT_ID": "2004-049A",
-    "EPOCH": "2025-03-26T05:19:34.116960",
-    "MEAN_MOTION": 15.00555103,
-    "ECCENTRICITY": 0.000583,
-    "INCLINATION": 98.3164,
-    "RA_OF_ASC_NODE": 103.8411,
-    "ARG_OF_PERICENTER": 20.5667,
-    "MEAN_ANOMALY": 339.5789,
-    "EPHEMERIS_TYPE": 0,
-    "CLASSIFICATION_TYPE": "U",
-    "NORAD_CAT_ID": 28492,
-    "ELEMENT_SET_NO": 999,
-    "REV_AT_EPOCH": 8655,
-    "BSTAR": 0.00048021,
-    "MEAN_MOTION_DOT": 0.00005995,
-    "MEAN_MOTION_DDOT": 0
+    OBJECT_NAME: sat["OBJECT_NAME"],
+    OBJECT_ID: "2004-049A",
+    EPOCH: "2025-03-26T05:19:34.116960",
+    MEAN_MOTION: 15.00555103,
+    ECCENTRICITY: 0.000583,
+    INCLINATION: 98.3164,
+    RA_OF_ASC_NODE: 103.8411,
+    ARG_OF_PERICENTER: 20.5667,
+    MEAN_ANOMALY: 339.5789,
+    EPHEMERIS_TYPE: 0,
+    CLASSIFICATION_TYPE: "U",
+    NORAD_CAT_ID: 28492,
+    ELEMENT_SET_NO: 999,
+    REV_AT_EPOCH: 8655,
+    BSTAR: 0.00048021,
+    MEAN_MOTION_DOT: 0.00005995,
+    MEAN_MOTION_DDOT: 0,
   };
   /*Error: react-stack-top-frame
     at exports.jsxDEV (http://localhost:3000/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=43031535:249:30)
     at http://localhost:3000/src/pages/Scenario.tsx?t=1757960147436:66:59*/
 
-  let satrec
-  console.log(sat)
-  satrec = json2satrec(sat as OMMJsonObjectV3)
+  let satrec;
+  // console.log(sat)
+  satrec = json2satrec(sat as OMMJsonObjectV3);
   // satrec = twoline2satrec(sat["TLE_LINE1"], sat["TLE_LINE2"]);
   // satrec = satellite.json2satrec(sat);
-  console.log(satrec)
+  // console.log(satrec)
   //to go from RAD/DAY -> REV/DAY: rad * 1440 * 0.159155
   //to go from REV/PER DAY to MINS/REV -> 1440/RevPerDay
   let totalIntervalsInDay = satrec.no * 1440 * 0.159155; //1440 = min && 0.159155 = 1turn
@@ -103,10 +105,9 @@ const satCzmlConverter = (
   }
   for (let i = 0; i <= 86400; i++) {
     //iterates every second (86400sec in 1day)
-    satrec = json2satrec(sat as OMMJsonObjectV3)
+    satrec = json2satrec(sat as OMMJsonObjectV3);
     let positionAndVelocity = sgp4(satrec, i * 0.0166667); // 0.0166667min = 1sec
-    if (positionAndVelocity == null)
-      continue;
+    if (positionAndVelocity == null) continue;
     let positionEci = positionAndVelocity.position;
     positionEci.x = positionEci.x * 1000;
     positionEci.y = positionEci.y * 1000;
@@ -136,7 +137,7 @@ const satCzmlConverter = (
       description: "Insert the altitude here??",
       label: {
         fillColor: {
-          rgba: [255, 0, 255, 255]
+          rgba: [255, 0, 255, 255],
         },
         font: "11pt Lucida Console",
         horizontalOrigin: "LEFT",
@@ -163,7 +164,7 @@ const satCzmlConverter = (
         material: {
           solidColor: {
             color: {
-              rgba: [255, 0, 255, 255]
+              rgba: [255, 0, 255, 255],
             },
           },
         },
@@ -175,7 +176,7 @@ const satCzmlConverter = (
         show: true,
         pixelSize: 10,
         color: {
-          rgba: [255, 0, 255, 255]
+          rgba: [255, 0, 255, 255],
         },
       },
       position: {
